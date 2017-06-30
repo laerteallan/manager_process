94b672341db8642494136a476b97a592f824ff3c
Laerte Allan <laerte.allan@gmail.com>
2017-06-30 17:10:25 -0300

    [new] manager_process module to high permance process
    This module was developed for high permance of the process.
    Based on the amount of processor in the machine or by the
    amount that user wants.

---
+++ manager_process.py
@@ -0,0 +1,110 @@
+"""Class responsable by multi-processing."""
+
+from multiprocessing import Process, cpu_count
+import signal
+import os
+from time import sleep
+ERROR_AMOUNT_PROCESS = """Higher than the existing process count.
+                          Msg org: Amount Process Exists: %s
+                                     Amount Process Terminate: %s
+                        """
+ERROR_PROCESS_NOT_FOUND = "Process not found"
+ERROR_PID_MUST_INTEGER = "The pid of the process must be an integer."
+
+
+PATH_SAVE_PID = "/var/run/"
+PATH_PROCESS_ACTIVE = "/proc/"
+PROCESS_PARENT = 'process.pid'
+CMD_PROC_CHILD = 'pgrep -P %s'
+TIMEOUT_DETROY_PROCESS = 0.1
+
+
+class ManagerProcesses(object):
+    """Class Administration multi-processing."""
+
+    def __init__(self):
+        """Class contructor."""
+        self.__list_process = []
+
+    def __create_process(self, p_amount_process=0, p_function=None,
+                         p_args=None):
+        """Method create process.
+
+        p_amount_process -> Amount create process
+        p_function -> Function to perform
+        p_args -> Tuple the Args to function
+        """
+        self.__save_pid_process(os.getpid(), PROCESS_PARENT)
+        for i in range(p_amount_process):
+            name_p = "process" + str(i) + '.pid'
+            process = Process(name=name_p, target=p_function,
+                              args=p_args)
+            self.__list_process.append(process)
+            process.start()
+            self.__save_pid_process(process.pid, name_p)
+
+    def __save_pid_process(self, p_pid, p_name_process):
+        """Method to save pid the process."""
+        file = open(PATH_SAVE_PID + p_name_process, "w")
+        file.write(str(p_pid))
+        file.close()
+
+    def __return_process_child(self, p_pid_process_parent):
+        """Method return process child."""
+        lst_proc_child = os.popen(CMD_PROC_CHILD % (p_pid_process_parent))
+        lst_proc_child = [pid_proc.strip('\n') for pid_proc in lst_proc_child]
+        return lst_proc_child
+
+    def create_process(self, p_create_cpu=False, p_amount_process=0,
+                       p_function=None, p_args=()):
+        """Method create most process."""
+        amount_process = p_amount_process
+        if p_create_cpu:
+            amount_process = cpu_count()
+        self.__create_process(amount_process, p_function, p_args)
+
+    def check_process_exist(self, p_pid):
+        """Method check process exist."""
+        if os.path.exists(PATH_PROCESS_ACTIVE + str(p_pid)):
+            return True
+
+        return False
+
+    def kill_all_process_parent_child(self):
+        """method to destroy all the parent and child processes."""
+        pid_parent = open(PATH_SAVE_PID + PROCESS_PARENT).read()
+        list_process_child = self.__return_process_child(pid_parent)
+
+        for pid_process_child in list_process_child:
+            if self.check_process_exist(pid_process_child):
+                self.kill_process_pid(int(pid_process_child))
+
+    def kill_process_pid(self, p_pid):
+        """Method kill process by PID."""
+        if not isinstance(p_pid, int):
+            raise Exception(ERROR_PID_MUST_INTEGER)
+
+        if self.check_process_exist(p_pid):
+            os.kill(p_pid, signal.SIGKILL)
+            return True
+        raise Exception(ERROR_PROCESS_NOT_FOUND)
+
+    def terminate_process(self, p_amount_process):
+        """Method terminate process.
+
+        p_amount_process - > Amount terminate Process created
+        """
+        amount_process = len(self.__list_process)
+        if p_amount_process > amount_process:
+            raise Exception(ERROR_AMOUNT_PROCESS % (str(amount_process),
+                                                    str(p_amount_process)))
+
+        for index_process in range(p_amount_process):
+            process = self.__list_process[index_process]
+            process.terminate()
+            sleep(TIMEOUT_DETROY_PROCESS)
+            process.exitcode == -signal.SIGTERM
+
+    def return_amount_Process_active(self):
+        """Return amount processes active."""
+        return len(self.__list_process)
